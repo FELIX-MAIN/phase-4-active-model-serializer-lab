088358e3d28c0f78a618939616d679429afcd1d9
FELIX-MAIN <felix.irungu@student.moringaschool.com>
2023-03-22 23:13:43 +0300

    Done

---
+++ app/serializers/author_posts_serializer.rb
@@ -0,0 +1,13 @@
+class AuthorPostsSerializer < ActiveModel::Serializer
+    attributes :title, :short_content
+    belongs_to :author
+    has_many :tags, serializer: TagSerializer
+  
+    def short_content
+      if object.content.length > 40
+        object.content[0..39] + '...'
+      else
+        object.content
+      end
+    end
+end
